fada3593d144bfa838bcc176762246c2647e2ab3
unknown <sureshmagix@gmail.com>
2022-10-02 17:45:49 +0530

    [MSG: Committed numbers and its data types in python]

---
+++ py_example3.py
@@ -0,0 +1,30 @@
+#python Numbers
+import random
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+print("First Random Number")
+print(random.randrange(1,10))
+print("Next Random number")
+
+print(random.randrange(1,10))
+print("Next Random number")
+print(random.randrange(1,10))
+print("Next Random number")
+print(random.randrange(1,10))
+print("Next Random number")
